646aad6b16b51893087a895e37d1e4bda6744f7a
Peyton Dexter <60048007+payday004@users.noreply.github.com>
2024-06-19 18:04:13 -0400

    added race table using optomistic UI

--- src/assets/RaceDataTable.tsx
+++ src/assets/RaceDataTable.tsx
@@ -5,10 +5,15 @@ import type { Schema } from "../../amplify/data/resource";
 import { generateClient } from "aws-amplify/data";
 import { useQuery } from "@tanstack/react-query";
 
+import {
+  Table,
+  TableCell,
+  TableRow,
+} from "@aws-amplify/ui-react";
+
 const client = generateClient<Schema>();
 
 function RaceDataTable() {
-  
   const {
     data: races,
     isLoading,
@@ -19,18 +24,25 @@ function RaceDataTable() {
     queryFn: async () => {
       const response = await client.models.Race.list();
 
-
       const allRaces = response.data;
-
 
       if (!allRaces) return null;
 
-
+      console.log(isLoading, isSuccess, isErrorQuery)
       return allRaces;
     },
   });
 
-  return <Button onClick={() => console.log({ races, isLoading, isSuccess, isErrorQuery })}>Click me</Button>;
+  return (
+    <Table>
+      {races?.map((race) => (
+        <TableRow key={race.id}>
+          <TableCell>{race.raceName} </TableCell>
+          <TableCell>{race.raceDay} </TableCell>
+        </TableRow>
+      ))}
+    </Table>
+  );
 }
 
 export default RaceDataTable;
